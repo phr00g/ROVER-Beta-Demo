f7ee76ffadb0b25c14eee9d1e3b8f09b6dec8e62
= <nickjalali@protonmail.com>
2023-06-11 13:08:01 -0700

    idek

---
+++ lang.py
@@ -0,0 +1,25 @@
+from classes import *
+from locations import *
+from items import *
+from tokenhandler import *
+import random
+import pygame
+import time
+import threading
+from pgmap import *
+
+
+decodeDict = {'⟐': 'a', '⟑': 'b', '⟒': 'c', '⟓': 'd', '⟔': 'e', '⟕': 'f', '⟖': 'g', '⟗': 'h', '⟘': 'i', '⟙': 'j', '⟚': 'k', '⟛': 'l', '⟜': 'm', '⟝': 'n', '⟞': 'o', '⟟': 'p', '⟠': 'q', '⟡': 'r', '⟢': 's', '⟣': 't', '⟤': 'u', '⟥': 'v', '⟦': 'w', '⟧': 'x', '⟨': 'y', '⟩': 'z', ' ': ' '}
+encodeDict = {'a': '⟐', 'b': '⟑', 'c': '⟒', 'd': '⟓', 'e': '⟔', 'f': '⟕', 'g': '⟖', 'h': '⟗', 'i': '⟘', 'j': '⟙', 'k': '⟚', 'l': '⟛', 'm': '⟜', 'n': '⟝', 'o': '⟞', 'p': '⟟', 'q': '⟠', 'r': '⟡', 's': '⟢', 't': '⟣', 'u': '⟤', 'v': '⟥', 'w': '⟦', 'x': '⟧', 'y': '⟨', 'z': '⟩', ' ': ' '}
+
+def decode(message):
+    decoded = ""
+    for char in message:
+        decoded += decodeDict[char]
+    return decoded
+
+def encode(message):
+    encoded = ""
+    for char in message:
+        encoded += encodeDict[char]
+    return encoded
