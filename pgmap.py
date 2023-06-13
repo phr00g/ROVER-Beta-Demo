d27e998e77d12be497b8f72c3328850681123539
= <nickjalali@protonmail.com>
2023-06-13 11:04:52 -0700

    changed pgmap to blink between blue and red, should change color and blink interval tho

--- pgmap.py
+++ pgmap.py
@@ -11,52 +11,50 @@ from lang import *
 def pygo():
     pygame.init()
     clock = pygame.time.Clock()
-    
 
-# Set the dimensions of the window
+    # Set the dimensions of the window
     window_width = 475
     window_height = 535
     tile_size = 50
     space_size = 10
-    
+
     # Define colors
     WHITE = (249, 244, 245)
     RED = (255, 0, 0)
-    BLACK = (46,49,56)
+    BLUE = (0, 0, 255)
+    BLACK = (46, 49, 56)
 
     # Create a Pygame window
     window = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("Game Map")
     clock = pygame.time.Clock()
-    running  =True
+    running = True
     window.fill(BLACK)
-    
+
+    blink_interval = 1.0  # 1 second interval for blinking
+    is_blinking = False
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
                 running = False
 
-    
-
         for x in range(len(map_array)):
-            
             for y in range(len(map_array[0])):
                 tile = map_array[x][y]
                 rect = pygame.Rect(x * (tile_size + space_size), y * (tile_size + space_size), tile_size, tile_size)
 
                 if tile == me.location:
-                    pygame.draw.rect(window, RED, rect)
+                    current_time = time.time()
+                    if current_time % blink_interval < blink_interval / 2:
+                        pygame.draw.rect(window, RED, rect)
+                    else:
+                        pygame.draw.rect(window, BLUE, rect)
                 else:
                     pygame.draw.rect(window, WHITE, rect)
 
         pygame.display.update()
         clock.tick(60)
-        
-        # Draw the map
-        
-
-        # Control the frame rate
     
 
 pygothread = threading.Thread(target=pygo)
